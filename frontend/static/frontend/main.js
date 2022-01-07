cad6d4b4f1d996b3b7e77814fc60484ad332c52e
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-07 07:49:15 -0800

    :hammer: edited leads.js reducer

--- frontend/static/frontend/main.js
+++ frontend/static/frontend/main.js
@@ -190,11 +190,11 @@
 /*!*******************************!*\
   !*** ./src/reducers/index.js ***!
   \*******************************/
-/*! exports provided: default */function(module,__webpack_exports__,__webpack_require__){"use strict";eval('__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var redux__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! redux */ "./node_modules/redux/es/redux.js");\n/* harmony import */ var _leads__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./leads */ "./src/reducers/leads.js");\n/* harmony import */ var _leads__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_leads__WEBPACK_IMPORTED_MODULE_2__);\n\n\n/* harmony default export */ __webpack_exports__["default"] = (Object(redux__WEBPACK_IMPORTED_MODULE_0__["combineReducers"])({\n  leads: _leads__WEBPACK_IMPORTED_MODULE_2___default.a\n}));\n\n//# sourceURL=webpack:///./src/reducers/index.js?')},"./src/reducers/leads.js":
+/*! exports provided: default */function(module,__webpack_exports__,__webpack_require__){"use strict";eval('__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var redux__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! redux */ "./node_modules/redux/es/redux.js");\n/* harmony import */ var _leads__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./leads */ "./src/reducers/leads.js");\n\n\n/* harmony default export */ __webpack_exports__["default"] = (Object(redux__WEBPACK_IMPORTED_MODULE_0__["combineReducers"])({\n  leads: _leads__WEBPACK_IMPORTED_MODULE_2__["default"]\n}));\n\n//# sourceURL=webpack:///./src/reducers/index.js?')},"./src/reducers/leads.js":
 /*!*******************************!*\
   !*** ./src/reducers/leads.js ***!
   \*******************************/
-/*! no static exports found */function(module,exports){eval("\n\n//# sourceURL=webpack:///./src/reducers/leads.js?")},"./src/store.js":
+/*! no exports provided */function(module,__webpack_exports__,__webpack_require__){"use strict";eval("__webpack_require__.r(__webpack_exports__);\n!(function webpackMissingModule() { var e = new Error(\"Cannot find module '../actions/types.js'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\n\n\n//# sourceURL=webpack:///./src/reducers/leads.js?")},"./src/store.js":
 /*!**********************!*\
   !*** ./src/store.js ***!
   \**********************/

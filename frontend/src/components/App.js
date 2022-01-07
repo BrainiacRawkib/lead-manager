64a969e57af81978bbe636a5bd2d66e8b5760b4a
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-06 23:12:47 -0800

    :tada: frontend app

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,19 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import ReactDOM from 'react-dom';
+
+import Header from "./layouts/Header";
+import Dashboard from "./leads/Dashboard";
 
 class App extends Component{
     render() {
-        return (<h1>Django React App</h1>)
+        return (
+            <Fragment>
+                <Header />
+                <div className="container">
+                    <Dashboard />
+                </div>
+            </Fragment>
+        )
     }
 }
 

499e18a8a7c3f26646385d4155bd6c7694965371
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-09 01:01:48 -0800

    :hammer: modified messages.js reducer

--- frontend/src/reducers/messages.js
+++ frontend/src/reducers/messages.js
@@ -4,8 +4,6 @@ const initialState = {};
 
 export default function (state = initialState, action) {
     switch (action.type) {
-        case GET_MESSAGES:
-            return action.payload
         case CREATE_MESSAGE:
             return (state = action.payload)
         default:

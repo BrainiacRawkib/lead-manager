8385da35ce33e3cecd576574f3c4066a6d5b8494
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-09 00:34:22 -0800

    :hammer: added messages in ADD_LEAD request

--- frontend/src/actions/leads.js
+++ frontend/src/actions/leads.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import {createMessage} from "./messages";
 
 import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from "./types";
 
@@ -30,6 +31,7 @@ export const deleteLead = (id) => dispatch => {
 export const addLead = (lead) => dispatch => {
     axios.post(`/api/leads/`, lead)
         .then((res) => {
+            dispatch(createMessage({leadAdded: "Lead Added"}));
             dispatch({
                 type: ADD_LEAD,
                 payload: res.data

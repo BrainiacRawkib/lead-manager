c987787654d097e7c7ac51888b878f3a4c978a93
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-09 10:38:02 -0800

    :hammer: configured REST_FRAMEWORK

--- lead_manager/settings.py
+++ lead_manager/settings.py
@@ -122,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# REST FRAMEWORK config
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'knox.auth.TokenAuthentication',
+    )
+}

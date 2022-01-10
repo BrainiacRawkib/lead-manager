5f8e633bb9ab309093060fc6f410a3be7953b8dc
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-10 07:14:48 -0800

    :hammer: created url path for LoginAPI() view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .api import RegisterAPI
+from .api import RegisterAPI, LoginAPI
 
 urlpatterns = [
     path('api/auth/', include('knox.urls')),
     path('api/auth/register/', RegisterAPI.as_view()),
+    path('api/auth/login/', LoginAPI.as_view()),
 ]

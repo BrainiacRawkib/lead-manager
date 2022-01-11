77940c2c17bc2c65ab996986f1da35b6a539cfe9
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-10 22:16:17 -0800

    :hammer: imported PrivateRoute.js in App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -10,6 +10,7 @@ import Dashboard from "./leads/Dashboard";
 import Alerts from "./layouts/Alerts";
 import Register from "./accounts/Register";
 import Login from "./accounts/Login";
+import PrivateRoute from "./common/PrivateRoute";
 
 import {Provider} from "react-redux";
 import store from "../store";
@@ -32,7 +33,7 @@ class App extends Component{
                             <Alerts />
                             <div className="container">
                                 <Switch>
-                                    <Route exact path="/" component={Dashboard} />
+                                    <PrivateRoute exact path="/" component={Dashboard} />
                                     <Route exact path="/register/" component={Register} />
                                     <Route exact path="/login/" component={Login} />
                                 </Switch>

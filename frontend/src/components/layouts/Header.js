fd9feb748658f4a4cbecc3462402486d6667c43f
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-10 18:10:05 -0800

    :hammer: modified Header.js

--- frontend/src/components/layouts/Header.js
+++ frontend/src/components/layouts/Header.js
@@ -13,7 +13,7 @@ class Header extends Component {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
                     <a className="navbar-brand" href="#">Lead Manager</a>
-                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
                         <li className="nav-item">
                             <Link to="/register/" className="nav-link">
                                 Register

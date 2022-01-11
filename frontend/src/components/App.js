f660c699e1da65c9eed643d90990d6bdfeb08b58
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-10 22:41:59 -0800

    :hammer: modified App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -14,6 +14,7 @@ import PrivateRoute from "./common/PrivateRoute";
 
 import {Provider} from "react-redux";
 import store from "../store";
+import {loadUser} from "../actions/auth";
 
 
 // Alert Options
@@ -23,6 +24,10 @@ const alertOptions = {
 }
 
 class App extends Component{
+    componentDidMount() {
+        store.dispatch(loadUser());
+    }
+
     render() {
         return (
             <Provider store={store}>

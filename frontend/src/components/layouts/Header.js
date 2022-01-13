0fecbc9dc8bd70c1e6983ae781c0c1b00ec56fb9
BrainiacRawkib <brainiacrawkib@gmail.com>
2022-01-12 23:30:38 -0800

    :hammer: added logout functionality in Header.js

--- frontend/src/components/layouts/Header.js
+++ frontend/src/components/layouts/Header.js
@@ -2,10 +2,12 @@ import React, {Component} from 'react';
 import {Link} from "react-router-dom";
 import {connect} from "react-redux";
 import PropTypes from 'prop-types';
+import {logout} from "../../actions/auth";
 
 class Header extends Component {
     static propTypes = {
-        auth: PropTypes.object.isRequired
+        auth: PropTypes.object.isRequired,
+        logout: PropTypes.func.isRequired
     }
 
     render() {
@@ -14,7 +16,7 @@ class Header extends Component {
         const authLinks = (
             <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
                 <li className="nav-item">
-                    <button className="nav-link btn btn-info btn-sm text-light">
+                    <button onClick={this.props.logout} className="nav-link btn btn-info btn-sm text-light">
                         Logout
                     </button>
                 </li>
@@ -60,4 +62,4 @@ const mapStateToProps = state => ({
     auth: state.auth
 })
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps, { logout })(Header);
